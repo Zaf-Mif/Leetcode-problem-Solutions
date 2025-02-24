3cfa1557543e1252554ea78988107ba700b99df7
Zaf-Mif <157963161+Zaf-Mif@users.noreply.github.com>
2025-02-24 22:49:59 +0300

    Created '3 Sum Closest' by A2SV Hub

---
+++ 24-Feb-2025/3 Sum Closest 282025.py
@@ -0,0 +1,20 @@
+# Problem: 3 Sum Closest - https://leetcode.com/problems/3sum-closest/description/
+
+class Solution:
+    def threeSumClosest(self, nums: List[int], target: int) -> int:
+        if not nums and len(nums)< 3:
+            return 0
+        nums.sort()
+
+        mx = float("inf")
+        for i in range(len(nums)-2):
+            left , right  = i+1 , len(nums) - 1
+            while left < right:
+                sm = nums[i] + nums[left] + nums[right]
+                if abs(sm-target) < abs(mx-target):
+                    mx = sm
+                elif sm < target:
+                    left += 1
+                else:
+                    right -= 1
+        return mx
