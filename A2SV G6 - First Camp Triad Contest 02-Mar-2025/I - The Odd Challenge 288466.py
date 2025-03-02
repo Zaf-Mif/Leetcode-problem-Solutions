bc5cc1fb65e2a8efff327d61365c72bbc0eb01a5
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-03-02 23:49:40 +0300

    Created 'I - The Odd Challenge' by A2SV Hub

---
+++ A2SV G6 - First Camp Triad Contest 02-Mar-2025/I - The Odd Challenge 288466.py
@@ -0,0 +1,17 @@
+# Problem: I - The Odd Challenge - https://codeforces.com/gym/589822/problem/I
+
+t = int(input())
+for i in range(t):
+    n , q = map(int,input().split())
+    a = list(map(int,input().split()))
+    ans = [0]
+    for k in range(n):
+        ans.append(ans[-1] + a[k])
+    for j in range(q):
+        l ,r , k = map(int,input().split())
+        res = ((ans[-1] - ans[r] ) + ans[l-1] ) + ((r-l+1)*k)
+        # print(ans , res)
+        if res % 2 == 0:
+            print("NO")
+        else:
+            print("YES")
