365033a0196b48feb7ea556c72f2def5fd7087a0
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-03-13 14:24:39 +0300

    Created 'Add Two Numbers' by A2SV Hub

---
+++ 13-Mar-2025/Add Two Numbers 304832.py
@@ -0,0 +1,45 @@
+# Problem: Add Two Numbers - https://leetcode.com/problems/add-two-numbers/
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def addTwoNumbers(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
+        dummy = ListNode()
+        self.current = dummy
+        self.carry = 0
+
+        def solve(list1 , list2 ):
+            if not list1 and not list2 and not self.carry:
+                return self.current
+
+            v1 = list1.val if list1 else 0
+            v2 = list2.val if list2 else 0
+
+            ans = v1 + v2 + self.carry
+
+            self.carry = ans // 10
+            ans = ans % 10
+                
+            self.current.next = ListNode(ans)
+            self.current = self.current.next
+
+            list1 = list1.next if list1 else None
+            list2 = list2.next if list2 else None
+            # print(self.current)
+
+            return solve(list1,list2)
+        solve(l1,l2)
+        return dummy.next
+        
+
+        
+        
+
+
+
+
+            
