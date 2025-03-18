91bbf59ec178c6c4ae6fc95ca67320645955562d
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-03-18 13:26:40 +0300

    Created 'Sort Characters By Frequency' by A2SV Hub

---
+++ 18-Mar-2025/Sort Characters By Frequency 310696.py
@@ -0,0 +1,16 @@
+# Problem: Sort Characters By Frequency - https://leetcode.com/problems/sort-characters-by-frequency/description/
+
+class Solution:
+    def largestNumber(self, nums: List[int]) -> str:
+        for i , num in enumerate(nums):
+            nums[i] = str(num)
+
+        def check(l1,l2):
+            if l1 + l2 > l2 + l1:
+                return -1
+            else :
+                return 1
+        
+        nums = sorted(nums, key = cmp_to_key(check))
+
+        return str(int("".join(nums)))
