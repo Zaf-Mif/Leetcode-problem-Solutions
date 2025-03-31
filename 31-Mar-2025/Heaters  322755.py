09f13d31329ff509612168e19240fa6477df3487
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-03-31 20:55:01 +0300

    Created 'Heaters ' by A2SV Hub

---
+++ 31-Mar-2025/Heaters  322755.py
@@ -0,0 +1,22 @@
+# Problem: Heaters  - https://leetcode.com/problems/heaters/
+
+from typing import List
+class Solution:
+    def findRadius(self, houses: List[int], heaters: List[int]) -> int:
+        houses.sort()
+        heaters.sort()
+        res = 0
+        for house in houses:
+            l , r = 0 , len(heaters)-1
+            mn = float('inf')
+
+            while l <= r:
+                m = (l+r) //2
+                mn = min(mn , abs(heaters[m]-house))
+                if heaters[m] < house:
+                    l = m+ 1
+                else:
+                    r = m -1
+
+            res = max(res, mn)
+        return res
