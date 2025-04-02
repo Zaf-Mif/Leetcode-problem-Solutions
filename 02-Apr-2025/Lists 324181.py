f767c3c82dc832c788fe3dbe4ceebbc5c3865313
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-04-02 19:50:46 +0300

    Created 'Lists' by A2SV Hub

---
+++ 02-Apr-2025/Lists 324181.py
@@ -0,0 +1,23 @@
+# Problem: Lists - https://www.hackerrank.com/challenges/python-lists/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    N= int(input())
+    res = []
+    for i in range (N):
+        s =input()
+        ss = s.split(" ")
+        if ss[0] == "insert":
+            res.insert(int(ss[1]) ,int(ss[2]))
+        elif ss[0] == "print":
+            print(res)
+        elif ss[0] == "remove":
+            if int(ss[1]) in res:
+                res.remove(int(ss[1]))
+        elif ss[0] == "append":
+            res.append(int(ss[1]))
+        elif ss[0] == "sort":
+            res.sort()
+        elif ss[0] == "pop":
+            res.pop()
+        elif ss[0] == "reverse":
+            res.reverse()
