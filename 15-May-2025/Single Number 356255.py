d25314c98794b383c53b2e56c784d80640fcada3
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-05-15 17:37:20 +0300

    Created 'Single Number' by A2SV Hub

---
+++ 15-May-2025/Single Number 356255.py
@@ -0,0 +1,9 @@
+# Problem: Single Number - https://leetcode.com/problems/single-number/
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        arrXor = 0
+        for num in nums:
+            arrXor ^= num
+        return arrXor
+        
