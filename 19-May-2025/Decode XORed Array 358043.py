55820084c16fe8702ed9b28fdbe6bace64d5b694
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-05-19 18:52:09 +0300

    Created 'Decode XORed Array' by A2SV Hub

---
+++ 19-May-2025/Decode XORed Array 358043.py
@@ -0,0 +1,9 @@
+# Problem: Decode XORed Array - https://leetcode.com/problems/decode-xored-array/description/
+
+class Solution:
+    def decode(self, encoded: List[int], first: int) -> List[int]:
+        # arr[i] ^ encoded [i]
+        arr = [first]
+        for num in encoded:
+            arr.append(arr[-1]^num)
+        return arr
