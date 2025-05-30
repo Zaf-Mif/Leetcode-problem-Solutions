783b1e185f5af57bd14bdb062c4cab36c0f03ae6
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-05-30 23:06:25 +0300

    Created 'Counting Sort' by A2SV Hub

---
+++ 30-May-2025/Counting Sort 361466.py
@@ -0,0 +1,9 @@
+# Problem: Counting Sort - https://www.hackerrank.com/challenges/countingsort1/problem
+
+def countingSort(arr):
+    # Write your code here
+    mx = max(arr)
+    res = [0]*(100)
+    for num in arr:
+        res[num] += 1
+    return res
