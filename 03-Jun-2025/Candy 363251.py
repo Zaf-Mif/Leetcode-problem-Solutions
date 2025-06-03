0b95c7e190062dba0c9fc5c770fa1d714023b5eb
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-06-03 22:01:04 +0300

    Created 'Candy' by A2SV Hub

---
+++ 03-Jun-2025/Candy 363251.py
@@ -0,0 +1,15 @@
+# Problem: Candy - https://leetcode.com/problems/candy/
+
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n=len(ratings)
+        candies=[1]*n
+        for i in range(n-1):
+            if ratings[i+1]>ratings[i]:
+                candies[i+1]=candies[i]+1
+        for i in range(n-1):
+            if ratings[n-1-(i+1)]>ratings[n-1-i] and candies[n-1-(i+1)]<=candies[n-1-i]:
+                candies[n-1-(i+1)]=candies[n-1-i]+1
+        print(candies)
+        
+        return sum(candies)
