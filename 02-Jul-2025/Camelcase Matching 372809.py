dac64323c44818647b80272629e6b3a843638fab
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-07-02 23:26:08 +0300

    Created 'Camelcase Matching' by A2SV Hub

---
+++ 02-Jul-2025/Camelcase Matching 372809.py
@@ -0,0 +1,11 @@
+# Problem: Camelcase Matching - https://leetcode.com/problems/camelcase-matching/
+
+class Solution:
+    def camelMatch(self, qs: List[str], p: str) -> List[bool]:
+        def v(s):
+            return [a for a in s if a.isupper()]
+
+        def IsUp(s, t):
+            it = iter(t)
+            return all(a in it for a in s)
+        return [v(p) == v(q) and IsUp(p, q) for q in qs]
