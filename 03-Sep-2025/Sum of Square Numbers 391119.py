7747d5b259b2a60ba5205d4074b38bf41926b322
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-09-03 22:03:40 +0300

    Created 'Sum of Square Numbers' by A2SV Hub

---
+++ 03-Sep-2025/Sum of Square Numbers 391119.py
@@ -0,0 +1,17 @@
+# Problem: Sum of Square Numbers - https://leetcode.com/problems/sum-of-square-numbers/
+
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        left = sm = 0
+        right = int(sqrt(c))
+        while left <= right:
+            sm = ((left*left) + (right*right))
+            print(sm)
+            if sm == c:
+                return True
+            elif sm < c:
+                left += 1
+            else:
+                right -= 1
+            
+        return False
