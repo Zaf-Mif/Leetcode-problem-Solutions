1b48ffcf6753428b44703dcf680c6d0c5df49b90
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-09-18 21:44:53 +0300

    Created 'Furthest Building You Can Reach' by A2SV Hub

---
+++ 18-Sep-2025/Furthest Building You Can Reach 393956.py
@@ -0,0 +1,21 @@
+# Problem: Furthest Building You Can Reach - https://leetcode.com/problems/furthest-building-you-can-reach/
+
+class Solution:
+    def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
+        n = len(heights)
+        pq = []
+        for i in range(n-1):
+            diff = heights[i+1] - heights[i]
+            if diff > 0:
+                if len(pq) < ladders:
+                    heappush(pq, diff)
+                else:
+                    if not pq or pq[0] >= diff:
+                        bricks -= diff
+                    else:
+                        poll = heappop(pq)
+                        heappush(pq, diff)
+                        bricks -= poll
+                    if bricks < 0:
+                        return i
+        return n-1
