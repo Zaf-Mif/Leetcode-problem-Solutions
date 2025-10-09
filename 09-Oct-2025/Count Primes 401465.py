9e5e6b8cb726bb7422a89407826ed717b435df59
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-10-09 16:23:52 +0300

    Created 'Count Primes' by A2SV Hub

---
+++ 09-Oct-2025/Count Primes 401465.py
@@ -0,0 +1,22 @@
+# Problem: Count Primes - https://leetcode.com/problems/count-primes/
+
+class Solution(object):
+    def countPrimes(self, n):
+        if n <= 2:
+            return 0
+        isPrime = [True for _ in range(n)]
+        isPrime[0] = isPrime[1] = False
+        i = 2
+        while i*i < n:
+            if isPrime[i]:
+                j = i * i
+                while j < n:
+                    isPrime[j] = False
+                    j += i
+            i += 1
+            
+        cnt = 0
+        for i in range(n):
+            if isPrime[i]:
+                cnt += 1
+        return cnt
