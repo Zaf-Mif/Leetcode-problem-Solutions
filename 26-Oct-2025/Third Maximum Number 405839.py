7d1927858680cbc9d441f81d49d9bcc514e2ba13
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-10-26 22:40:37 +0300

    Created 'Third Maximum Number' by A2SV Hub

---
+++ 26-Oct-2025/Third Maximum Number 405839.py
@@ -0,0 +1,5 @@
+# Problem: Third Maximum Number - https://leetcode.com/problems/third-maximum-number/description/
+
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        return (sorted(set(nums)))[-3] if len(set(nums)) >= 3 else max(nums)
