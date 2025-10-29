bead223afec59b5ccfc0b909ff1eb4d6227eecfc
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-10-29 22:28:53 +0300

    Created 'Vowels of All Substrings' by A2SV Hub

---
+++ 29-Oct-2025/Vowels of All Substrings 406348.py
@@ -0,0 +1,11 @@
+# Problem: Vowels of All Substrings - https://leetcode.com/problems/vowels-of-all-substrings/
+
+class Solution:
+    def countVowels(self, word: str) -> int:
+        n = len(word)
+        ans = 0
+        vowel = 'aeiou'
+        for i in range(n):
+            if word[i] in vowel:
+                ans += ((i+1) * (n-i))
+        return ans 
