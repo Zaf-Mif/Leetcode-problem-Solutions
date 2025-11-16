bcb6ef9521bb7cdfbb5a9e28a0e32ea231a1602d
Zaferan Miftah <157963161+Zaf-Mif@users.noreply.github.com>
2025-11-16 23:46:36 +0300

    Created 'Sum of All Subset XOR Totals' by A2SV Hub

---
+++ 16-Nov-2025/Sum of All Subset XOR Totals 409075.py
@@ -0,0 +1,12 @@
+# Problem: Sum of All Subset XOR Totals - https://leetcode.com/problems/sum-of-all-subset-xor-totals/
+
+class Solution:
+    def subsetXORSum(self, nums: List[int]) -> int:
+        
+        def dfs(i , total):
+            if i == len(nums):
+                return total
+            
+            return dfs(i + 1 , total ^ nums[i]) +dfs(i + 1 , total) 
+        
+        return dfs(0, 0)
